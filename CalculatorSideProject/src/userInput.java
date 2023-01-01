1adb038286f37653bf648a7263579d3fd6783d3e
Andrew Kallai <andrewka@udel.edu>
2023-01-01 14:50:42 -0500

    Added textbox and window functionality

--- CalculatorSideProject/src/userInput.java
+++ CalculatorSideProject/src/userInput.java
@@ -1,9 +1,27 @@
-public class userInput
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+
+public class userInput extends JFrame
 {
-	private int mortgagePercentage;
-	private int minimumScoreRequirement;
-	
-	public static void main(String[] args)
-	{
-	}
+private int mortgagePercentage;
+private int minimumScoreRequirement;
+
+public userInput()
+{
+	super("Mortgage Calculator");
+	initWindow();
+	setSize(480, 300);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+public void initWindow()
+{
+	JTextField textField = new JTextField("This is a text", 20);
+	setLayout(new FlowLayout());
+	add(textField);
+}
+public static void main(String[] args)
+{
+	new userInput().setVisible(true);
+}
 }
