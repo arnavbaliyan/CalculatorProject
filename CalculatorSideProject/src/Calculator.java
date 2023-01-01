72055f81476c7562cc4b605c18b9cb915f14e85e
arnavbaliyan <arnavbaliyan@arnavs-air>
2023-01-01 09:10:44 -0500

    2nd commit.

--- CalculatorSideProject/src/Calculator.java
+++ CalculatorSideProject/src/Calculator.java
@@ -4,6 +4,9 @@ public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		//Test comment.
+		int x = 2 + 2;
+		int y = x * 2;
+		System.out.println(y);
 	}
 
 }
