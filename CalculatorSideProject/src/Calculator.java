a81d87b8f9c95eeedab00ab46aa349f27da1d266
Andrew Kallai <andrewka@udel.edu>
2023-01-01 18:58:21 -0500

    Added monthly payment feature to Calculator.java

--- CalculatorSideProject/src/Calculator.java
+++ CalculatorSideProject/src/Calculator.java
@@ -1,12 +1,20 @@
+//import java.math;
+public class Calculator
+{
 
-public class Calculator {
+public static double monthlyMortgagePayment(int downPayment, double interestRate, int timePeriod)
+{
+	double m= (downPayment * interestRate) / (1 - Math.pow(1 + interestRate, -timePeriod));
+	return Math.round(m* 100) / 100;
+}
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-		//Test comment.
-		int x = 2 + 2;
-		int y = x * 2;
-		System.out.println(y+"<-Value of y\n"+x+"<-Value of x");
-	}
-
+public static void main(String[] args)
+{
+	//int p=userInput.getDownPayment();
+	//double r=userInput.getInterestRate();
+	//r =(r/100)/12;
+	//int t=userInput.getTimePeriod();
+	//t=t*12;
+	//System.out.println("Monthly Payment: "+monthlyMortgagePayment(p,r,t));	
+}
 }
