a6d9b9ad9791b3c62fa84cac4db243074e3715b0
Andrew Kallai <andrewka@udel.edu>
2023-01-12 15:00:21 -0500

    Simplified userInput.java.

--- CalculatorSideProject/src/userInput.java
+++ CalculatorSideProject/src/userInput.java
@@ -1,27 +1,22 @@
-//package main;
-
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-
+import java.awt.Font;
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class userInput extends JFrame implements ActionListener 
 {
-
-private JButton calculate;
-private JLabel downPayment;
-private JLabel loanAmount;
-private JLabel loanTerm;
-private JLabel interestRate;
-private JLabel numberOfMonths;
-private JLabel payment;
-private Container c;
 private JTextField DPInput;
 private JTextField LAInput;
 private JTextField LTInput;
 private JTextField IRInput;
 private JTextField NOMInput;
 private JTextField PaymentOutput;
-
+/*
+ * Constructor which initializes Calculator Window in GUI
+ */
 public userInput() 
 {
 super("Loan Calculator");
@@ -29,58 +24,60 @@ initWindowComponents();
 setSize(500,500);
 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
-
+/*
+ * Initializes textboxes and buttons in the Calculator Window
+ */
 private void initWindowComponents() 
 {
 
-Font font = new Font(Font.SANS_SERIF,  Font.BOLD, 21);
-c = this.getContentPane();
+Font font = new Font(Font.SANS_SERIF, Font.BOLD, 21);
+Container c = this.getContentPane();
 c.setLayout(null);
 
-calculate = new JButton("Calculate");
+JButton calculate = new JButton("Calculate");
 calculate.setSize(100,50);
 calculate.setLocation(195,390);
 c.add(calculate);
 calculate.addActionListener(this);
 
 
-downPayment = new JLabel("Down Payment");
+JLabel downPayment = new JLabel("Down Payment");
 downPayment.setSize(200,100);
 downPayment.setFont(font);
 c.add(downPayment);
 
-loanAmount = new JLabel("Loan Amount");
+JLabel loanAmount = new JLabel("Loan Amount");
 loanAmount.setSize(200,100);
 loanAmount.setLocation(0,60);
 loanAmount.setFont(font);
 c.add(loanAmount);
 
-loanTerm = new JLabel("Loan Term");
+JLabel loanTerm = new JLabel("Loan Term");
 loanTerm.setSize(200,100);
 loanTerm.setLocation(0,120);
 loanTerm.setFont(font);
 c.add(loanTerm);
 
-interestRate = new JLabel("Interest Rate");
+JLabel interestRate = new JLabel("Interest Rate");
 interestRate.setSize(200,100);
 interestRate.setLocation(0,180);
 interestRate.setFont(font);
 c.add(interestRate);
 
-numberOfMonths = new JLabel("Months");
+JLabel numberOfMonths = new JLabel("Months");
 numberOfMonths.setSize(200,100);
 numberOfMonths.setLocation(0,240);
 numberOfMonths.setFont(font);
 c.add(numberOfMonths);
 
-payment = new JLabel("Payment:");
+JLabel payment = new JLabel("Payment:");
 payment.setSize(100,100);
 payment.setLocation(100,300);
 payment.setFont(font);
 c.add(payment);
 
 //Text field box for Down Payment input
- DPInput = new JTextField(20);
+DPInput = new JTextField(20);
 DPInput.setBounds(260,40,165,25);
 c.add(DPInput);
 
@@ -103,27 +100,24 @@ c.add(IRInput);
 NOMInput = new JTextField(20);
 NOMInput.setBounds(260,280,165,25);
 c.add(NOMInput);
-
+//Text field to display monthly payment
 PaymentOutput = new JTextField(20);
 PaymentOutput.setBounds(197,340,165,25);
 PaymentOutput.setEditable(false);
 c.add(PaymentOutput);
 
 }
-
-public void actionPerformed(ActionEvent evt) 
-{
-	int downPayment = Integer.parseInt(DPInput.getText());
-	double interestRate = Double.parseDouble(IRInput.getText());
-	int timePeriod= Integer.parseInt(NOMInput.getText());
-	System.out.println("Payment: "+Calculator.monthlyMortgagePayment(downPayment,interestRate,timePeriod));
+	/*
+ 	* Action event for when the calulate button is clicked
+ 	*/
+	public void actionPerformed(ActionEvent evt){ 
+	  int downPayment = Integer.parseInt(DPInput.getText());
+	  double interestRate = Double.parseDouble(IRInput.getText());
+	  int timePeriod= Integer.parseInt(NOMInput.getText());
+	  PaymentOutput.setText(""+Calculator.monthlyMortgagePayment(downPayment,interestRate,timePeriod));
+	}
+	//Calculator display is run
+	public static void main(String[] args){ 
+	  new userInput().setVisible(true);
+	}
 }
-
-public static void main(String[] args) 
-{	
-new userInput().setVisible(true);
-}
-
-
-}
-
