1c7db13abc7a012c883d9adc85ce3b2042743337
Andrew Kallai <andrewka@udel.edu>
2023-01-13 14:39:53 -0500

    Resultant calculation from Calculator is rounded to two decimal places

--- CalculatorSideProject/src/Calculator.java
+++ CalculatorSideProject/src/Calculator.java
@@ -1,17 +1,13 @@
 public class Calculator{
 	/*
-	 *
+	 *Calculates payment per month using the principle, yearly interest rate, and years as inputs
 	 */
 	public static double monthlyMortgagePayment(int downPayment, double interestRate, int timePeriod){
 	  interestRate = interestRate/(12*100);
 	  timePeriod = timePeriod * 12;
   	  double m= (downPayment * interestRate) / (1 - Math.pow(1 + interestRate, -timePeriod));
           System.out.println(downPayment+"\n"+interestRate+"\n"+timePeriod);
-  	  double numerator = downPayment*interestRate;
-  	  double denominator = (1-Math.pow(1+interestRate,-timePeriod));
-  	  System.out.println(numerator+"\n"+denominator);
-	  return m;
-  	  //return Math.round(m* 100) / 100;
+  	  return Math.round(m* 100) / 100.0;
 	}
 	public static void main(String[] args){
 	  System.out.println(monthlyMortgagePayment(200000,6.5,30)); 
